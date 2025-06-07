adf27a995710d8422aefdaa639732324991ca650
Shivam03-coder <shivam850anand@gmail.com>
2025-06-07 12:00:19 +0530

    admin pannel started

---
+++ server/src/controller/admin.controller.ts
@@ -0,0 +1,13 @@
+import { db } from "@src/db";
+import { GlobalUtils } from "@src/global";
+import AuthServices from "@src/services/auth";
+import {
+  ApiError,
+  ApiResponse,
+  AsyncHandler,
+} from "@src/utils/server-functions";
+import { Request, Response } from "express";
+
+export class AdminController {
+
+}
